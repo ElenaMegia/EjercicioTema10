05e6f2df9c1d4cc53ef112583801a07aaab83202
ElenaMegia <emegican@myuax.com>
2023-03-18 18:07:22 +0100

    Ejercic1 terminado

---
+++ src/main/java/org/example/exceptions/DesbordaCapacidadException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class DesbordaCapacidadException extends Exception{
+    public DesbordaCapacidadException(String mensaje) {
+
+        super(mensaje);
+    }
+}
